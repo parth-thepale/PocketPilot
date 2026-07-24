8ddb0cbe6454a806d4097c18bf7af5f93e0ce352
Parth Thepale <parth.24one@gmail.com>
2026-07-24 20:28:20 +0530

    Sprint 8.3: Custom premium checkbox

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+
+
+console.log("🚀 PocketPilot v0.1 Loaded Successfully!");
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+
+            target.scrollIntoView({
+
+                behavior: "smooth"
+
+            });
+
+        }
+
+    });
+
+});
